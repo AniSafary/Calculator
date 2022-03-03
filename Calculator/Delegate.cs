99bdb03e9550dab8f20e8e545792e41db5169625
Ani <safan88@gmail.com>
2022-03-03 14:08:33 +0400

    Add project files.

---
+++ Calculator/Delegate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Delegate
+    {
+
+        private delegate double Օperation(double a, double b);
+
+        //public delegate Օperation Calculate(Օperation @calcualtor, char op);
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Sub(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Mult(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Div(double a, double b)
+        {
+            return a / b;
+        }
+
+        //public static Օperation Calculated(Օperation @cal, char op)
+        //{
+
+        //    switch (op)
+        //    {
+        //        case '+':
+        //            @cal = Add;
+        //            break;
+        //        case '*':
+        //            @cal = Mult;
+        //            break;
+        //        case '/':
+        //            @cal = Div;
+        //            break;
+        //        case '-':
+        //            @cal = Sub;
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //    return @cal;
+        //}
+    }
+}
---
+++ Calculator/Read.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Read
+    {   
+        private double first;
+        private double second;
+        private char operation;
+
+        public Read()
+        {
+
+            while (StringSplit(Console.ReadLine()))
+            { }
+        }
+
+        private bool StringSplit(string str)
+        {
+            first = 0;
+            second = 0;
+
+            string currentnumber = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != '+' && str[i] != '-' && str[i] != '*' && str[i] != '/'&&(str[i])>='0'&&str[i]<='9'||str[i]=='.')
+                {
+                    currentnumber += str[i];
+                }
+                else
+                {
+                    operation = str[i];
+                    first = double.Parse(currentnumber);
+                    currentnumber = "";
+                }                
+            }
+            second= double.Parse(currentnumber);
+            //Console.WriteLine("first  " + first + "   second  " + second);
+
+            Result();
+            return true;
+        }
+
+        private void Result()
+        {
+            double result=0;
+            switch (operation)
+            {
+                case '+':
+                    result=Delegate.Add(first, second);
+                    break;
+                case '*':
+                    result = Delegate.Mult(first, second);
+                    break;
+                case '/':
+                    result = Delegate.Div(first, second);
+                    break;
+                case '-':
+                    result = Delegate.Sub(first, second);
+                    break;
+                default:
+                    Console.WriteLine("Wrong Action");
+                    break;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
